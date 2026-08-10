1ad0ef7b4eeb6675edad3348b65cc64cf6eadb0b
Moneer Abedrabbo <Mabedrabbo10@gmail.com>
2026-08-10 02:32:06 -0400

    Rebuild home page, sync social constants, clean up blog posts

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const GITHUB_URL = 'https://github.com/MKabedrabbo';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/moneer-abedrabbo-086293343/';
+export const EMAIL = 'mabedrabbo10@gmail.com';
